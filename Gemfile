d46deae089a50e02736cd50444f609a64950f4e5
Hanna <github email address>
2017-03-19 12:42:07 -0400

    rack cors gem

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'rails_12factor', group: :production
 
 gem 'pry'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,7 @@
 class PeopleController < ApplicationController
   # protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token
-  before_action :before_headers
-  after_action :after_headers
+  # skip_before_filter :verify_authenticity_token
+
 
   def index
     @people = People.all
@@ -75,21 +74,4 @@ class PeopleController < ApplicationController
       params.permit(:name, :favoriteCity)
     end
 
-    def before_headers
-      if request.method == 'OPTIONS'
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-        headers['Access-Control-Max-Age'] = '1728000'
-
-        render :text => '', :content_type => 'text/plain'
-      end
-    end
-
-    def after_headers
-      headers['Access-Control-Allow-Origin'] = "*"
-      headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-      headers['Access-Control-Max-Age'] = "1728000"
-    end
 end
