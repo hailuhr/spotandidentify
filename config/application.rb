a791c618ea4d7f1a36f2a1fdc3d7717ebec6a44d
Hanna <github email address>
2017-03-19 12:48:50 -0400

    allows localhost and heroku access to api

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,11 @@ module Spotandidentify
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'localhost:3000', 'spotter-client.herokuapp.com'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
-    
+
   end
 end
