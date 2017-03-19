6db42c584c7cd52647dff61b64d70a01a37af35c
Hanna <github email address>
2017-03-19 12:26:15 -0400

    syntax error

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -75,7 +75,7 @@ class PeopleController < ApplicationController
 
     def set_header
       headers['Access-Control-Allow-Origin'] = "*"
-      headers['Access-Control-Request-Method'] => %w{GET POST OPTIONS}.join(",")
+      headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
       # config.action_dispatch.default_headers = {
       #   'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
       #   'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
