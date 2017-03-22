7460024fbcca1299ad4df758dc8c522e952bb33d
Hanna <github email address>
2017-03-21 23:21:50 -0400

    removed non working redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,4 @@ class ApplicationController < ActionController::Base
   end
 
 
-    rescue_from ActionController::RoutingError do |exception|
-    flash[:error] = "There is no such person with that id"
-    redirect_to root_url
-  end
-
-
 end
