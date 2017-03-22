13f72068ed599d0631aba33a6de29f5d1d745d0c
Hanna <github email address>
2017-03-21 23:17:41 -0400

    redirects for wrong url addresses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,7 @@ class ApplicationController < ActionController::Base
   def home
   end
 
-  rescue_from ActionController::RoutingError do |exception|
-    flash[:error] = "There is no such person with that id"
-    redirect_to root_url
-  end
+
 
   def redirect
     flash[:error] = "There is no such person with that id"
