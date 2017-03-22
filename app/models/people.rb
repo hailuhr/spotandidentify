f307b201c42ef2ab55d9302a84e03bc9246f73ca
Hanna <github email address>
2017-03-21 21:15:12 -0400

    capitalized method for names before save"

--- app/models/people.rb
+++ app/models/people.rb
@@ -2,4 +2,14 @@ class People < ApplicationRecord
 
   validates_presence_of :name, :favoriteCity
 
+  before_save :capitalize_name_and_city
+
+
+  def capitalize_name_and_city
+    if (self.name && !self.name.blank?) && (self.favoriteCity && !self.favoriteCity.blank?)
+      self.name = self.name.split.collect(&:capitalize).join(' ')
+      self.favoriteCity = self.favoriteCity.split.collect(&:capitalize).join(' ')
+    end
+  end
+
 end
